b50f3a7db9695117290b33204cc411cd15bfe073
Isaac Chung <48971969+isaac-chung@users.noreply.github.com>
2022-10-31 20:28:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Strava Kudos Giver 👍👍👍
+
+[![Python 3.7](https://img.shields.io/badge/python-3.7-blue.svg)](https://www.python.org/downloads/release/python-370/) 
+
+A Python tool to automatically give Strava Kudos to recent activities on your feed. There are a few repos that uses JavaScript like [strava-kudos-lambda](https://github.com/mjad-org/strava-kudos-lambda) and [strava-kudos](https://github.com/rnvo/strava-kudos). 
+
+## 🏃 Usage
+1. Fork the repo
+2. Setup the libraries and environment
+3. Start giving kudos by running:
+```
+python3 give_kudos.py
+```
+
+## 🛠️Setup
+
+### Playwright
+[Playwright](https://github.com/microsoft/playwright-python) is used, so be sure to follow instructions to install it properly. 
+
+### Environment Variables
+
+Set the environment variables for your email and password as follows:
+```
+export STRAVA_EMAIL=YOUR_EMAIL
+export STRAVA_PASSWORD=YOUR_PASSWORD
+```
+
+## 🔬Testing
+Manual testing was done in Python 3.7.6. 
+
+## Contributions
+Let me know if you wish to add anything or if there are any issues!
+
+[![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)](https://GitHub.com/Naereen/)
---
+++ give_kudos.py
@@ -0,0 +1,84 @@
+import os
+import time
+
+from playwright.sync_api import sync_playwright
+
+BASE_URL = "https://www.strava.com/"
+
+class KudosGiver:
+    """
+    Logins into Strava and gives kudos to all activities under
+    Following. Additionally, scrolls down to check for more activities
+    until no more kudos can be given at this time.
+    """
+    def __init__(self, max_retry_scroll=3) -> None:
+        self.EMAIL = os.environ.get('STRAVA_EMAIL')
+        self.PASSWORD = os.environ.get('STRAVA_PASSWORD')
+
+        if self.EMAIL is None or self.PASSWORD is None:
+            raise Exception(f"Must set environ variables EMAIL AND PASSWORD. \
+                e.g. run export STRAVA_EMAIL=YOUR_EMAIL")
+
+        self.max_retry_scroll = max_retry_scroll
+        self.kudos_button_pattern = '[data-testid="kudos_button"]'
+        p = sync_playwright().start()
+        self.browser = p.firefox.launch() # does not work in chrome
+        self.page = self.browser.new_page()
+
+
+    def email_login(self):
+        """
+        Login using email and password
+        """
+        self.page.goto(os.path.join(BASE_URL, 'login'))
+        self.page.fill('#email', self.EMAIL)
+        self.page.fill("#password", self.PASSWORD)
+        self.page.click("button[type='submit']")
+        print("---Logged in!!---")
+        self.page.goto(os.path.join(BASE_URL, "dashboard"), wait_until="domcontentloaded")
+        
+
+    def locate_kudos_buttons_and_maybe_give_kudos(self, button_locator) -> int:
+        """
+        input: playwright.locator class
+        Returns count of kudos given.
+        """
+        b_count= button_locator.count()
+        given_count = 0
+        for i in range(b_count):
+            button = button_locator.nth(i)
+            if button.get_by_test_id("unfilled_kudos").count():
+                button.click()
+                given_count += 1
+                time.sleep(1)
+        print(f"Kudos given: {given_count}")
+        return given_count
+
+    def give_kudos(self):
+        """
+        Scroll through pages to give kudos that are giveable.
+        """
+        ## Give Kudos on loaded page ##
+        button_locator = self.page.locator(self.kudos_button_pattern)
+        kudos_given = self.locate_kudos_buttons_and_maybe_give_kudos(button_locator=button_locator)
+        curr_retry = self.max_retry_scroll
+
+        ## Scroll down and repeat ##
+        while kudos_given or curr_retry: 
+            self.page.mouse.wheel(0, 12000)
+            time.sleep(5)
+            kudos_given = self.locate_kudos_buttons_and_maybe_give_kudos(button_locator=button_locator)
+            curr_retry -= 1
+        
+        print("That's all, folks! Terminating... ")
+        self.browser.close()
+        
+
+def main():
+    kg = KudosGiver()
+    kg.email_login()
+    kg.give_kudos()
+
+
+if __name__ == "__main__":
+    main()

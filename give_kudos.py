12d9a7465736624ef3a7cc81c273ddc48120ed60
Isaac Chung <chungisaac1217@gmail.com>
2024-08-31 20:00:50 +0300

    Try checking page url login and test (#26)
    * split into smaller functions
    
    * try testing build and run
    
    * move login confirmation within the method
    
    * all PRs
    
    * try old workflow
    
    * use old worklfow only

--- give_kudos.py
+++ give_kudos.py
@@ -22,13 +22,14 @@ class KudosGiver:
         self.start_time = time.time()
         self.num_entries = 100
         self.web_feed_entry_pattern = '[data-testid=web-feed-entry]'
+        self.own_profile_id = ""
 
         p = sync_playwright().start()
         self.browser = p.firefox.launch() # does not work in chrome
         self.page = self.browser.new_page()
 
 
-    def email_login(self):
+    def _login_by_email(self):
         """
         Login using email and password
         """
@@ -54,8 +55,21 @@ class KudosGiver:
             print("Using CSS selector (using ID)", e)
 
         self.page.get_by_role("button", name='Log In').click()
+        self._get_dashboard_and_scroll()
+
+        ## confirm logged in
+        if "login" in self.page.url:
+            print(self.page.url)
+            raise Exception("Not logged in.")
+        
         print("---Logged in!!---")
-        self._run_with_retries(func=self._get_page_and_own_profile)
+
+    def email_login(self):
+        """ 
+        Login with retries. Confirm logged in before getting own profile.
+        """
+        self._run_with_retries(self._login_by_email)
+        self._get_own_profile()
         
     def _run_with_retries(self, func, retries=3):
         """
@@ -70,10 +84,7 @@ class KudosGiver:
             except:
                 time.sleep(1)
 
-    def _get_page_and_own_profile(self):
-        """
-        Limit activities count by GET parameter and get own profile ID.
-        """
+    def _get_dashboard_and_scroll(self):
         self.page.goto(os.path.join(BASE_URL, f"dashboard?num_entries={self.num_entries}"))
 
         ## Scrolling for lazy loading elements.
@@ -82,6 +93,11 @@ class KudosGiver:
             time.sleep(0.5)
             self.page.keyboard.press('PageUp')
 
+
+    def _get_own_profile(self):
+        """
+        Limit activities count by GET parameter and get own profile ID.
+        """
         try:
             self.own_profile_id = self.page.locator(".user-menu > a").get_attribute('href').split("/athletes/")[1]
         except:

c33065bb9c017042e8900049468b634f220a9b2f
Isaac Chung <chungisaac1217@gmail.com>
2024-11-26 07:07:33 +0000

    add back try-except for cookies

--- give_kudos.py
+++ give_kudos.py
@@ -33,6 +33,10 @@ class KudosGiver:
         Login using email and password
         """
         self.page.goto(os.path.join(BASE_URL, 'login'))
+        try:
+            self.page.get_by_role("button", name="Reject").click(timeout=5000)
+        except:
+            pass
         self.page.fill('#email', self.EMAIL)
         self.page.fill("#password", self.PASSWORD)
         self.page.click("button[type='submit']")
